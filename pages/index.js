1cefd995aed3acd626a6ccc416d9919b33cc282f
v-Alqorith <vahapcandongel@hotmail.com>
2023-02-27 19:23:49 +0000

    need to add loading bar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Welcomer from "@/components/Welcomer";
+
 export default function Home() {
   return (
     <div className="bg-dark-bg w-full h-screen">
-
+      <Welcomer />
     </div>
   );
 }
